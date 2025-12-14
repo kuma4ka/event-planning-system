3d89a831b9418c1aee8e6b64ce4175ccef27de69
Dmytro Prokopenko <portal200385@gmail.com>
2025-12-14 19:30:18 +0100

    feat(app): extend venue service with crud operations for admin

---
+++ src/EventPlanning.Application/DTOs/CreateVenueDto.cs
@@ -0,0 +1,9 @@
+﻿namespace EventPlanning.Application.DTOs;
+
+public record CreateVenueDto(
+    string Name,
+    string Address,
+    int Capacity,
+    string? Description,
+    string? ImageUrl
+);
--- src/EventPlanning.Application/Validators/CreateGuestDtoValidator.cs
+++ src/EventPlanning.Application/Validators/CreateGuestDtoValidator.cs
@@ -8,8 +8,11 @@ public class CreateGuestDtoValidator : AbstractValidator<CreateGuestDto>
     public CreateGuestDtoValidator()
     {
         RuleFor(x => x.FirstName).NotEmpty().MaximumLength(50);
+        
         RuleFor(x => x.LastName).NotEmpty().MaximumLength(50);
+        
         RuleFor(x => x.Email).NotEmpty().EmailAddress();
+        
         RuleFor(x => x.EventId).NotEmpty();
     }
 }
