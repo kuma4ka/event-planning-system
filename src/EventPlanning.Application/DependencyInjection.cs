f055712745a6844d19857ef2c6b221df7cfb19ba
Dmytro Prokopenko <portal200385@gmail.com>
2025-12-14 18:24:49 +0100

    feat(app): implement venue service and dto for dropdown usage

--- src/EventPlanning.Application/DependencyInjection.cs
+++ src/EventPlanning.Application/DependencyInjection.cs
@@ -12,6 +12,7 @@ public static class DependencyInjection
         services.AddValidatorsFromAssemblyContaining<Validators.CreateEventDtoValidator>();
 
         services.AddScoped<IEventService, EventService>();
+        services.AddScoped<IVenueService, VenueService>();
 
         return services;
     }
---
+++ src/EventPlanning.Application/Interfaces/IVenueService.cs
@@ -0,0 +1,8 @@
+﻿using EventPlanning.Application.DTOs;
+
+namespace EventPlanning.Application.Interfaces;
+
+public interface IVenueService
+{
+    Task<List<VenueDto>> GetVenuesAsync(CancellationToken cancellationToken = default);
+}
