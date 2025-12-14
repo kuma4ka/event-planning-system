77403c725fd9f8913e8842ee5e9210f50417479f
Dmytro Prokopenko <portal200385@gmail.com>
2025-12-14 18:01:24 +0100

    feat(app): implement update and delete event logic with validation and UI

---
+++ src/EventPlanning.Application/Validators/UpdateEventDtoValidator.cs
@@ -0,0 +1,25 @@
+﻿using EventPlanning.Application.DTOs;
+using FluentValidation;
+
+namespace EventPlanning.Application.Validators;
+
+public class UpdateEventDtoValidator : AbstractValidator<UpdateEventDto>
+{
+    public UpdateEventDtoValidator()
+    {
+        RuleFor(x => x.Id).NotEmpty();
+        
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Event name is required.")
+            .MaximumLength(100);
+
+        RuleFor(x => x.Description)
+            .NotEmpty().WithMessage("Description is required.")
+            .MaximumLength(500);
+
+        RuleFor(x => x.Date)
+            .GreaterThan(DateTime.Now).WithMessage("Event date cannot be in the past.");
+
+        RuleFor(x => x.Type).IsInEnum();
+    }
+}
