98ca4a378fbc9f7f32d57475f1012d3fae0aaa4d
Dmytro Prokopenko <portal200385@gmail.com>
2025-12-20 20:43:12 +0100

    feat(core): add pagination models and search dto

---
+++ src/EventPlanning.Domain/Models/PagedList.cs
@@ -0,0 +1,28 @@
+﻿namespace EventPlanning.Domain.Models;
+
+public class PagedList<T>
+{
+    public List<T> Items { get; private set; }
+    public int PageNumber { get; private set; }
+    public int TotalPages { get; private set; }
+    public int TotalCount { get; private set; }
+
+    public bool HasPreviousPage => PageNumber > 1;
+    public bool HasNextPage => PageNumber < TotalPages;
+
+    public PagedList(List<T> items, int count, int pageNumber, int pageSize)
+    {
+        TotalCount = count;
+        PageSize = pageSize;
+        PageNumber = pageNumber;
+        TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+        Items = items;
+    }
+
+    public int PageSize { get; private set; }
+
+    public static PagedList<T> Create(List<T> source, int count, int pageNumber, int pageSize)
+    {
+        return new PagedList<T>(source, count, pageNumber, pageSize);
+    }
+}
