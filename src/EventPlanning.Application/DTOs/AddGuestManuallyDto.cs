6827acf1d02b33d0e85e94d69147f543428c920b
Dmytro Prokopenko <portal200385@gmail.com>
2025-12-20 23:14:12 +0100

    feat(app): implement manual guest addition logic for organizers

---
+++ src/EventPlanning.Application/DTOs/AddGuestManuallyDto.cs
@@ -0,0 +1,9 @@
+﻿namespace EventPlanning.Application.DTOs;
+
+public record AddGuestManuallyDto(
+    int EventId,
+    string FirstName,
+    string LastName,
+    string Email,
+    string? PhoneNumber
+);
