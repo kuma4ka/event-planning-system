d54042ee0e725189e30012157675504c48e09b9c
Dmytro Prokopenko <portal200385@gmail.com>
2025-12-25 15:18:07 +0100

    Refactor Venue dto: add inheritance to CreateVenueDto.cs

---
+++ src/EventPlanning.Application/DTOs/Venue/CreateVenueDto.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace EventPlanning.Application.DTOs.Venue;
+
+public record CreateVenueDto(
+    string Name,
+    string Address,
+    int Capacity,
+    string? Description,
+    IFormFile? ImageFile
+) : VenueBaseDto(Name, Address, Capacity, Description, ImageFile);
