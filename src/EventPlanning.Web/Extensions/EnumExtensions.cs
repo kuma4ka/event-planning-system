0e0b38d482a999407f5a6877fcc623f292cd05b3
Dmytro Prokopenko <portal200385@gmail.com>
2025-12-25 13:44:35 +0100

    feat(backend): add EnumExtensions helper for display names and update ViewImports

---
+++ src/EventPlanning.Web/Extensions/EnumExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace EventPlanning.Web.Extensions;
+
+public static class EnumExtensions
+{
+    public static string GetDisplayName(this Enum enumValue)
+    {
+        var field = enumValue.GetType().GetField(enumValue.ToString());
+        var attribute = field?.GetCustomAttribute<DisplayAttribute>();
+        
+        return attribute?.Name ?? enumValue.ToString();
+    }
+}
