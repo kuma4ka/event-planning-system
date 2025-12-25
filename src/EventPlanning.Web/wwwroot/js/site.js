ee82a29751d7f95e388c50b0bb91afb0bca45f48
Dmytro Prokopenko <portal200385@gmail.com>
2025-12-25 13:44:47 +0100

    fix(ui): resolve modal scrolling issue by moving modal out of loop and refactoring JS to ES modules

--- src/EventPlanning.Web/wwwroot/js/site.js
+++ src/EventPlanning.Web/wwwroot/js/site.js
@@ -1,4 +1,7 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿import { initDashboard } from './components/dashboard.js';
 
-// Write your JavaScript code.
+document.addEventListener('DOMContentLoaded', () => {
+
+    initDashboard();
+    console.log('Stanza JS loaded modules successfully.');
+});
