1165ce3c30b70501cc53201ee56d6ce59ab1945c
Dmytro Prokopenko <portal200385@gmail.com>
2025-12-28 00:17:00 +0100

    refactor(client): modularize event actions and cleanup site.js. Extract delete modal and copy link logic to event-actions component.

--- src/EventPlanning.Web/wwwroot/js/site.js
+++ src/EventPlanning.Web/wwwroot/js/site.js
@@ -1,12 +1,14 @@
 ﻿import { initDashboard } from './components/dashboard.js';
 import { initVenueManagement } from './components/admin/venues.js';
-import { initEventDetails } from './components/event-details.js';
+import { initEventDetails } from './components/event/event-details.js';
+import {initEventActions} from "./components/event/event-actions.js";
 
 document.addEventListener('DOMContentLoaded', () => {
 
     initDashboard();
     initVenueManagement();
     initEventDetails();
+    initEventActions()
 
     console.log('Stanza JS loaded modules successfully.');
 });
