f2e09075be708e918029fb7ae6cf25356c2cc892
Dmytro Prokopenko <portal200385@gmail.com>
2025-12-28 00:16:08 +0100

    feat(common): add SelectListItem extension for Enum display names

---
+++ src/EventPlanning.Web/Extensions/SelectListItemExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace EventPlanning.Web.Extensions;
+
+public static class SelectListItemExtensions
+{
+    public static IEnumerable<SelectListItem> ToSelectList<TEnum>(this TEnum? selectedValue, string defaultText = "All") 
+        where TEnum : struct, Enum
+    {
+        var items = new List<SelectListItem>();
+
+        if (!string.IsNullOrEmpty(defaultText))
+        {
+            items.Add(new SelectListItem
+            {
+                Value = "",
+                Text = defaultText,
+                Selected = !selectedValue.HasValue
+            });
+        }
+
+        var enumItems = Enum.GetValues<TEnum>().Select(e => new SelectListItem
+        {
+            Value = e.ToString(),
+            Text = e.GetDisplayName(),
+            Selected = selectedValue.HasValue && e.Equals(selectedValue.Value)
+        });
+
+        items.AddRange(enumItems);
+
+        return items;
+    }
+}
