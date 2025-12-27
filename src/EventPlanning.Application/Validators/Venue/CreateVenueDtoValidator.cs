1893d0688ed9335f45da5e2c93b184678b346567
Dmytro Prokopenko <portal200385@gmail.com>
2025-12-27 13:13:55 +0100

    refactor(validation): extract constants and add capacity limit. Centralize validation rules in ValidationConstants. Add MaxCapacity (20000). Update base validator to use constants.

--- src/EventPlanning.Application/Validators/Venue/CreateVenueDtoValidator.cs
+++ src/EventPlanning.Application/Validators/Venue/CreateVenueDtoValidator.cs
@@ -1,4 +1,5 @@
 ﻿using EventPlanning.Application.DTOs.Venue;
+using FluentValidation;
 
 namespace EventPlanning.Application.Validators.Venue;
 
@@ -6,5 +7,7 @@ public class CreateVenueDtoValidator : VenueBaseDtoValidator<CreateVenueDto>
 {
     public CreateVenueDtoValidator()
     {
+        RuleFor(x => x.ImageFile)
+            .NotNull().WithMessage("Image is required when creating a venue.");
     }
 }
