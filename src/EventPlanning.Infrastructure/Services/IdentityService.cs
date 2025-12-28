816c36466a802463e563564de114cc72742406fa
Dmytro Prokopenko <portal200385@gmail.com>
2025-12-28 02:51:18 +0100

    feat(infra): implement IIdentityService for user retrieval

---
+++ src/EventPlanning.Infrastructure/Services/IdentityService.cs
@@ -0,0 +1,13 @@
+﻿using EventPlanning.Application.Interfaces;
+using EventPlanning.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+
+namespace EventPlanning.Infrastructure.Services;
+
+public class IdentityService(UserManager<User> userManager) : IIdentityService
+{
+    public async Task<User?> GetUserByIdAsync(string userId)
+    {
+        return await userManager.FindByIdAsync(userId);
+    }
+}
