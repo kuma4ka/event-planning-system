7c6dfdfec1ea9161f4bb4d80f72c09c5739956d0
Dmytro Prokopenko <portal200385@gmail.com>
2025-12-31 00:54:13 +0100

    fix(app): update max_capacity for venus

--- src/EventPlanning.Application/Constants/ValidationConstants.cs
+++ src/EventPlanning.Application/Constants/ValidationConstants.cs
@@ -7,7 +7,7 @@ public static class ValidationConstants
     public const int MaxDescriptionLength = 1000;
     public const int MaxImageSizeBytes = 5 * 1024 * 1024; // 5 MB
     public const int MaxImageSizeInMb = 5;
-    public const int MaxCapacity = 20000;
+    public const int MaxCapacity = 100_000;
     
     public static readonly string[] AllowedImageContentTypes = ["image/jpeg", "image/png", "image/jpg", "image/webp"];
 }
