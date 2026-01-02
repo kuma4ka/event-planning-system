da9b2c62d194c2b25a50a0b9138fce4eb07ffd59
Dmytro Prokopenko <portal200385@gmail.com>
2026-01-03 00:18:35 +0100

    refactor(app): remove unused AddGuestAsync from IGuestService

--- src/EventPlanning.Application/Interfaces/IGuestService.cs
+++ src/EventPlanning.Application/Interfaces/IGuestService.cs
@@ -4,7 +4,7 @@ namespace EventPlanning.Application.Interfaces;
 
 public interface IGuestService
 {
-    Task AddGuestAsync(Guid userId, CreateGuestDto dto, CancellationToken cancellationToken = default);
+
 
     Task AddGuestManuallyAsync(Guid currentUserId, AddGuestManuallyDto dto,
         CancellationToken cancellationToken = default);
