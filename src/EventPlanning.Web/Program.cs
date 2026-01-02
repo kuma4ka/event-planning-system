896c2fbb8f99a4306473dec50b05be3a32b7d9a9
Dmytro Prokopenko <portal200385@gmail.com>
2026-01-02 13:50:16 +0100

    feat(security): add rate limiting to login and register actions

--- src/EventPlanning.Web/Program.cs
+++ src/EventPlanning.Web/Program.cs
@@ -31,6 +31,20 @@ builder.Services.AddRateLimiter(options =>
         limiterOptions.QueueProcessingOrder = System.Threading.RateLimiting.QueueProcessingOrder.OldestFirst;
         limiterOptions.QueueLimit = 0;
     });
+    options.AddFixedWindowLimiter("login-limit", limiterOptions =>
+    {
+        limiterOptions.PermitLimit = 5;
+        limiterOptions.Window = TimeSpan.FromMinutes(10);
+        limiterOptions.QueueProcessingOrder = System.Threading.RateLimiting.QueueProcessingOrder.OldestFirst;
+        limiterOptions.QueueLimit = 2;
+    });
+    options.AddFixedWindowLimiter("register-limit", limiterOptions =>
+    {
+        limiterOptions.PermitLimit = 3;
+        limiterOptions.Window = TimeSpan.FromHours(1);
+        limiterOptions.QueueProcessingOrder = System.Threading.RateLimiting.QueueProcessingOrder.OldestFirst;
+        limiterOptions.QueueLimit = 0;
+    });
 });
 
 try
